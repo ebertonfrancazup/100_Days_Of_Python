27ad7557f916bb449030580533d3922592fe3ce0
Éberton França <eberton.franca@zup.com.br>
2024-07-12 15:13:16 -0300

    :rocket: update day1

--- Day_1/fstrings.py
+++ Day_1/fstrings.py
@@ -4,7 +4,7 @@ peso = 90
 imc = peso / altura ** 2
 
 #f-strings -- Inserir variáveis com strings
-resultado = f'{nome} tem {altura:.2f} de altura e seu IMC é: {imc}'
+resultado = f'{nome} tem {altura:.2f} de altura e seu IMC é: {imc:.2f}'
 print(resultado)
 
 # .format() Formatando as variávies no padrão desejado
@@ -13,8 +13,17 @@ b = 'B'
 c = 1.1
 
 # Entre {} é esperado algum argumento que são passados pela variável formato
-string = 'a={} b={} c{:.2f}'
+##string = 'a={} b={} c{:.2f}'
 
-formato = string.format(a,b,c)
+#formato = string.format(a,b,c)
 
-print(formato)
+#print(formato)
+
+string2 = 'a={0} b={1} c={2:.2f}'
+formato2 = string2.format(a, b, c)
+print(formato2)
+
+nome = "Luiz"
+idade = 23
+formato3 = '{1} tem {0} anos'
+print(formato3.format(nome, idade))
---
+++ Day_1/operadores.py
@@ -0,0 +1,14 @@
+condicao = True
+condicao2 = False
+condicao3 = True
+
+if condicao == False: 
+    
+    print("Verdadeiro")
+
+elif condicao2 == True:
+    
+    print("Falso")
+else:
+
+    print("Nenhum e nem outro")
